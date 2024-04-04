d6846bb90c6f9776fc0c198188c078554d42afca
devinw50 <addicteddmg@gmail.com>
2024-04-04 10:07:27 -0400

    thr
    rthtr

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -8,14 +8,6 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
 </template>
 
 <style scoped>
@@ -35,10 +27,4 @@ h3 {
   text-align: center;
 }
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref,  } from 'vue';
+import {onBeforeMount} from 'vue';
+
+const thing = ref('')
+async function thingy () {
+  try {
+  let fetchthing = await fetch ("https://data.cityofnewyork.us/resource/pitm-atqc.json")
+  let putasjson = await fetchthing.json();
+  thing.value = putasjson;
+  console.log(putasjson)
+}  
+catch (error) {
+  console.log ("error", error)
+}}
+onBeforeMount(() =>{
+    thingy();
+})
+
 </script>
+
 
 <template>
   <main>
     <TheWelcome />
   </main>
-</template>
+</template>l
